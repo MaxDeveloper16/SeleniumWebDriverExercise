338065ed4a7e04ee1b6debaaa5c22b15cab79af4
Max <maxdeveloper16@gmail.com>
2020-08-21 23:02:02 -0400

    SwitchToActiveWindow test

--- test/SwitchToActiveWindow.java
+++ test/SwitchToActiveWindow.java
@@ -12,6 +12,17 @@ public class SwitchToActiveWindow {
 
         driver.get("https://formy-project.herokuapp.com/switch-window");
 
+        WebElement newTabButton = driver.findElement(By.id("new-tab-button"));
+        newTabButton.click();
+
+        String originalHandle = driver.getWindowHandle();
+
+        for(String handle1: driver.getWindowHandles()) {
+            driver.switchTo().window(handle1);
+        }
+
+        driver.switchTo().window(originalHandle);
+
         driver.quit();
     }
 }
