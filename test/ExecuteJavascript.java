e5b2801cb94c512242518a2b6b7786a1d2b10b28
Max <maxdeveloper16@gmail.com>
2020-09-01 22:38:53 -0400

    ExecuteJavascript test

--- test/ExecuteJavascript.java
+++ test/ExecuteJavascript.java
@@ -13,6 +13,13 @@ public class ExecuteJavascript {
 
         driver.get("https://formy-project.herokuapp.com/modal");
 
+        WebElement modalButton = driver.findElement(By.id("modal-button"));
+        modalButton.click();
+
+        WebElement closeButton = driver.findElement(By.id("close-button"));
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].click();", closeButton);
+        
         driver.quit();
     }
 }
